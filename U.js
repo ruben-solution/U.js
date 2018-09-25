eab516d970902fdf0036bc5377ea7d4aaccac15c
ruben-solution <ruben.allenspach@solution.ch>
2018-09-25 15:44:22 +0200

    added function: range

--- U.js
+++ U.js
@@ -334,6 +334,35 @@ var U = (function() {
             var rdmString = "";
             for (; rdmString.length < len; rdmString += Math.random().toString(36).substr(2));
             return rdmString.substr(0, len);
+        },
+
+        /**
+         * Generate an integer Array containing an arithmetic progression
+         * 
+         * @param {int} start
+         * @param {int} stop
+         * @param {int} step
+         * 
+         * @returns {Array}
+         */
+        range: function(start, stop, step) {
+            if (stop == null) {
+                stop = start || 0;
+                start = 0;
+            }
+
+            if (!step) {
+                step = stop < start ? -1 : 1;
+            }
+
+            var length = Math.max(Math.ceil((stop - start) / step), 0);
+            var range = Array(length);
+
+            for (var idx = 0; idx < length; idx++, start += step) {
+                range[idx] = start;
+            }
+
+            return range;
         }
     }
 })();
